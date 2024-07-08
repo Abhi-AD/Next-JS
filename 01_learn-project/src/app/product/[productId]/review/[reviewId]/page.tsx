35bd0fce3624bb3cd4a7dd5d3c0f7230314a8540
Abhi-AD <dangiabhi332@gmail.com>
2024-07-08 07:37:22 +0545

    fix: Add Custom Not Found Page

--- 01_learn-project/src/app/product/[productId]/review/[reviewId]/page.tsx
+++ 01_learn-project/src/app/product/[productId]/review/[reviewId]/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import { notFound } from "next/navigation";
 export default function ReviewDetail({ params }: {
      params: {
           productId: string;
           reviewId: string;
      }
 }) {
+     if (parseInt(params.reviewId) > 1000) {
+          notFound();
+     }
      return (
           <>
-               <h2>Reviews {params.reviewId} for {params.productId}</h2>
+               <h2>Reviews {params.reviewId} for Product {params.productId}</h2>
           </>
      )
 }
--- 01_learn-project/src/app/product/review/[reviewId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ReviewDetail({ params }: { params: { productId: string; reviewId: string } }) {
-     return (
-          <>
-               <p>Reviews {params.reviewId} for {params.productId}</p>
-          </>
-     );
-}
