e6e0b794e8ee8f87f2917e3e096f8dae444599fc
Abhi-AD <dangiabhi332@gmail.com>
2024-07-08 07:55:22 +0545

    fix: Add File Colocation and Private Folders

---
+++ 01_learn-project/src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+function BarChart() {
+     return (
+          <>
+               <h1>Bar Chart !</h1>
+          </>
+     )
+}
+
+export default function Dashboard() {
+     return (
+          <>
+               <h1>Welcome to Dashboard Page !</h1>
+          </>
+     )
+}
