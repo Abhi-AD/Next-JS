be6573f07b73a8e21eb29396a2eb1266396ec9e9
Abhi-AD <dangiabhi332@gmail.com>
2024-07-10 08:02:27 +0545

    fix: Add Recovering from Errors Concept

--- 01_learn-project/src/app/product/[productId]/review/[reviewId]/error.tsx
+++ 01_learn-project/src/app/product/[productId]/review/[reviewId]/error.tsx
@@ -1,4 +1,4 @@
 "use client";
-export default function ErrorBoundary({ error }: { error: Error }) {
-     return (<h1>{error.message}</h1>)
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void; }) {
+     return (<h1>{error.message} <button onClick={reset}>Try again</button></h1>)
 }
