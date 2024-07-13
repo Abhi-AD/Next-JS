5de1fcc02a96aafdd6cf46d3608616f2c6f7fb39
Abhi-AD <dangiabhi332@gmail.com>
2024-07-13 21:59:04 +0545

    fix: Add Handling Errors in layouts Concept

---
+++ 01_learn-project/src/app/product/error.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void; }) {
+     return (<h1>{error.message} <button onClick={reset}>Try again</button></h1>)
+}
