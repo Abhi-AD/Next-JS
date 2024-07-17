157d7c654b0d131ebd5bf118f7bc5864c9ed438a
Abhi-AD <dangiabhi332@gmail.com>
2024-07-17 21:39:28 +0545

    fix:Add Cookies in Route Handlers  Concept

--- 02_route-handlers/src/app/profile/api/route.ts
+++ 02_route-handlers/src/app/profile/api/route.ts
@@ -1,14 +1,20 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
      const requestHeaders = new Headers(request.headers);
      const headerList = headers();
+     cookies().set("resultPerPage", "20")
+     const theme = request.cookies.get("theme");
      console.log(requestHeaders.get("Authorization"))
      console.log(headerList.get("Authorization"))
+     console.log(theme)
+     console.log(cookies().get("resultPerPage"));
+     cookies()
      return new Response("<h1>Profile API Data!</h1>", {
           headers: {
-               "Content-type": "text/html"
+               "Content-type": "text/html",
+               "Set-cookie": "theme=dark",
           }
      })
 }
